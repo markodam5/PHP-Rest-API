f06d27b9d87eb75401af79ef7e7f1c13e92d59ac
Marko Damjanac <36675797+markodam5@users.noreply.github.com>
2025-12-31 08:05:28 +0100

    Change database name from rest_users to rest_api_db

--- db.php
+++ db.php
@@ -3,11 +3,12 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$dbname = "rest_users";
+$dbname = "rest_api_db";
 
 try{
     $pdo = new PDO("mysql:host=$host;dbname=$dbname",$user,$password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e){
     die("Connection failed: " . $e->getMessage());
+
 }
